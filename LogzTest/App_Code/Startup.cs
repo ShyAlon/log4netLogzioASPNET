7d4b1fdff65d6555a8c321e5bd520ebe1b5f8f32
Shy Alon <shy.alon@gmail.com>
2018-10-03 16:20:48 +0300

    An example project

---
+++ LogzTest/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogzTest.Startup))]
+namespace LogzTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ LogzTest/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        XmlConfigurator.Configure();
+
+        var logger = LogManager.GetLogger("GreetingsLogger");
+
+        logger.Info("ASP.NET");
+       
+    }
+}
